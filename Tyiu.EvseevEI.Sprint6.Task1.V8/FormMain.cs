3f7a002915b6ad08fa28d7b0f41178b6c6581e8f
Евсеев Егор <Tyuiu.EvseevEI@outlook.com>
2024-12-10 01:48:26 +0500

    1

---
+++ Tyiu.EvseevEI.Sprint6.Task1.V8/FormMain.cs
@@ -0,0 +1,10 @@
+namespace Tyiu.EvseevEI.Sprint6.Task1.V8
+{
+    public partial class FormMain : Form
+    {
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+    }
+}
