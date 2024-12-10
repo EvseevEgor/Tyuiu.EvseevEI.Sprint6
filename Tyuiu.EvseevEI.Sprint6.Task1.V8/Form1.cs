ce95b3225f28d35944024be15b55ece7ebbcd3fe
Евсеев Егор <Tyuiu.EvseevEI@outlook.com>
2024-12-10 13:13:26 +0500

    1

---
+++ Tyuiu.EvseevEI.Sprint6.Task1.V8/Form1.cs
@@ -0,0 +1,10 @@
+namespace Tyuiu.EvseevEI.Sprint6.Task1.V8
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
