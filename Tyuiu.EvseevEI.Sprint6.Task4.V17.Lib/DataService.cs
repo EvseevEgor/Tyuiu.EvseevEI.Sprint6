f5f8dc2956a4a1a76648a94ae453c7768962d0e6
Евсеев Егор <Tyuiu.EvseevEI@outlook.com>
2024-12-10 19:03:31 +0500

    4

---
+++ Tyuiu.EvseevEI.Sprint6.Task4.V17.Lib/DataService.cs
@@ -0,0 +1,32 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint6;
+namespace Tyuiu.EvseevEI.Sprint6.Task4.V17.Lib
+{
+    public class DataService : ISprint6Task4V17
+    {
+        public double[] GetMassFunction(int startValue, int stopValue)
+        {
+            int len = stopValue - startValue + 1;
+            double[] valueArray = new double[len];
+            int count = 0;
+            double y;
+            for (int x = startValue; x <= stopValue; x++)
+            {
+                if (x + 1.7 != 0)
+                {
+                    y = Math.Round(((Math.Sin(x) / (x + 1.7)) - Math.Cos(x) * 4 * x - 6), 2);
+
+                }
+                else
+                {
+                    y = 0;
+                }
+                valueArray[count] = y;
+                count++;
+            }
+            return valueArray;
+            {
+
+            }
+        }
+    }
+}
